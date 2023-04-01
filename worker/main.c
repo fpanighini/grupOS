8f521480dd60066348d5f6862d0a0405966f7d87
Lucas Perri <lperri@itba.edu.ar>
2023-04-01 16:59:50 -0300

    Added md5sum to worker

--- worker/main.c
+++ worker/main.c
@@ -1,5 +1,64 @@
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MD5_LEN 32
+
+void close_pipe(int pipe_fd[2])
+{
+    close(pipe_fd[STDOUT_FILENO]);
+    close(pipe_fd[STDIN_FILENO]);
+}
+
+int md5sum(char *hash, char *const path)
+{
+    int pipe_fd[2];
+    if (pipe(pipe_fd) == -1)
+    {
+        return -1;
+    }
+
+    int pid = fork();
+    if (pid == -1)
+    {
+        close_pipe(pipe_fd);
+        return -1;
+    }
+
+    // child code
+    if (pid == 0)
+    {
+        dup2(pipe_fd[STDOUT_FILENO], STDOUT_FILENO);
+        char *const md5_path = "/usr/bin/md5sum";
+        char *const argv[3] = {md5_path, path, NULL};
+        execv(md5_path, argv);
+        _exit(1);
+    }
+
+    int status;
+    if (waitpid(pid, &status, 0) == -1 || !WIFEXITED(status) || WEXITSTATUS(status) != 0)
+    {
+        close_pipe(pipe_fd);
+        return -1;
+    }
+
+    if (read(pipe_fd[STDIN_FILENO], hash, MD5_LEN) != MD5_LEN)
+    {
+        close_pipe(pipe_fd);
+        return -1;
+    }
+
+    return 0;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    /* test code */
+    char hash[MD5_LEN];
+    md5sum(hash, "/usr/bin/md5sum");
+    if (write(STDOUT_FILENO, hash, MD5_LEN) != MD5_LEN)
+    {
+        return 1;
+    }
+    /* test code */
     return 0;
 }
